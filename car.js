cbce51b1da01e25682610391a58796c5ef1db43e
theoofman <tyler@elsoutlet.com>
2022-05-15 11:56:55 -0700

    Worked on Sensor

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
   update() {
     this.#move();
+    this.sensor.update();
   }
   #move() {
     if (this.controls.forward) {
@@ -64,5 +66,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
--- main.js
+++ main.js
@@ -3,8 +3,7 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
@@ -12,7 +11,12 @@ function animate() {
   car.update();
 
   canvas.height = window.innerHeight;
+
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
   road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   requestAnimationFrame(animate);
 }
